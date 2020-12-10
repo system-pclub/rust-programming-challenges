0ea0ff134107401f8b39dc441e4e7b582588dafa
Ryvian <31315697+Ryvian@users.noreply.github.com>
2020-12-10 15:34:38 +0800

    30273850 and 2 before

---
+++ 39827244/buggy.rs
@@ -0,0 +1,23 @@
+mod case2 {
+    struct Foo {}
+
+    struct Bar2<'b> {
+        x: &'b Foo,
+    }
+
+    impl<'b> Bar2<'b> {
+        fn f(&'b mut self) -> &'b Foo {
+            self.x
+        }
+    }
+
+    fn f4() {
+        let foo = Foo {};
+        let mut bar2 = Bar2 { x: &foo };
+        bar2.f(); // (3) 'bar2' is borrowed as mutable, but who borrowed it?
+        let z = bar2.f(); // error: cannot borrow `bar2` as mutable more than once 
+                          // at a time [E0499]
+    }
+}
+
+fn main() {}
