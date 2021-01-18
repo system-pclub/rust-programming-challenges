9f966db9d2b3cbcee92a66d1510e98aa390f2575
Ryvian <31315697+Ryvian@users.noreply.github.com>
2021-01-18 22:27:17 +0800

    61002805

---
+++ 61002805/fixed.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let foo = vec![('a', vec![1,2,3]), ('v', vec![2,3,4])];
+    let baz: Vec<(char, i32)> = foo
+        .into_iter()
+        .map(|a| a.1.clone().into_iter().map(move |b| (a.0, b)))
+        .flatten()
+        .collect();
+    println!("{:?}", baz);
+}
