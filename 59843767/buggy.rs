1503bdf55561e9596364bd87c2400ba41fde26ac
Ryvian <31315697+Ryvian@users.noreply.github.com>
2021-01-28 14:13:35 +0800

    59843767

---
+++ 59843767/buggy.rs
@@ -0,0 +1,25 @@
+pub struct Foo<'a, T> {
+    cb: Box<dyn FnMut(Vec<T>) + 'a>,
+}
+
+impl<'a, T> Foo<'a, T> {
+    pub fn new<F: FnMut(Vec<T>) + 'a>(cb: F) -> Self {
+        Self { cb: Box::new(cb) }
+    }
+
+    pub fn fun(&mut self, v: T) {
+        let vector = vec![v];
+        (self.cb)(vector);
+    }
+}
+
+fn main() {
+    let mut a = Vec::new();
+    let mut foo = Foo::new(move |v| {
+        for i in v {
+            a.push(i);
+        }
+    });
+    foo.fun(1);
+    println!("{:?}", a);
+}
