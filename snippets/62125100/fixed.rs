ee7936cae697e34c84b40600dfe3d20eea1f0588
Ryvian <31315697+Ryvian@users.noreply.github.com>
2021-03-29 22:13:13 +0800

    add fix for 62125100

---
+++ snippets/62125100/fixed.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let x = vec![1, 2, 3];
+    
+    let equal_to_x = |z| z == x;
+    
+    println!("{:?}", x);
+    
+    let y = vec![1, 2, 3];
+    
+    assert!(equal_to_x(y));
+}
